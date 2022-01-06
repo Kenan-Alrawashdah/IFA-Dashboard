9a81d8fcfb6623f63211854a740d4f371801fc97
Muradawad20 <68800448+MuradAL-Tarifi@users.noreply.github.com>
2022-01-07 01:58:19 +0200

    ..

--- src/app/components/store/routting/store.module.ts
+++ src/app/components/store/routting/store.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { StoreRoutingModule } from './store-routing.module';
-import { FormsModule } from '@angular/forms';
-import { NbCardModule, NbUserModule, NbButtonModule, NbTabsetModule, NbActionsModule, NbRadioModule, NbSelectModule, NbListModule, NbIconModule } from '@nebular/theme';
-import { NgxEchartsModule } from 'ngx-echarts';
-import { ThemeModule } from '../../../@theme/theme.module';
-
+import { StoreRoutingModule } from "./store-routing.module";
+import { FormsModule } from "@angular/forms";
+import {
+  NbCardModule,
+  NbUserModule,
+  NbButtonModule,
+  NbTabsetModule,
+  NbActionsModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbListModule,
+  NbIconModule,
+} from "@nebular/theme";
+import { NgxEchartsModule } from "ngx-echarts";
+import { ThemeModule } from "../../../@theme/theme.module";
+import { StoreComponent } from "../store.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [StoreComponent],
   imports: [
     CommonModule,
     StoreRoutingModule,
@@ -26,6 +36,6 @@ import { ThemeModule } from '../../../@theme/theme.module';
     NbIconModule,
     NbButtonModule,
     NgxEchartsModule,
-  ]
+  ],
 })
-export class StoreModule { }
+export class StoreModule {}
--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, TemplateRef } from "@angular/core";
+import { NbTabsetModule } from "@nebular/theme";
+import { NbDialogService } from "@nebular/theme";
 
 @Component({
-  selector: 'ngx-store',
-  templateUrl: './store.component.html',
-  styleUrls: ['./store.component.scss']
+  selector: "ngx-store",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./store.component.html",
+  styleUrls: ["./store.component.scss"],
 })
 export class StoreComponent implements OnInit {
+  constructor(private dialogService: NbDialogService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  open(dialog: TemplateRef<any>) {
+    this.dialogService.open(dialog, {
+      context: "this is some additional data passed to dialog",
+    });
   }
-
+  
 }
